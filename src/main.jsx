af1b2958de3650e993fa1c42638e2d0879ed3213
shmaruf58 <maruf414100masum@gmail.com>
2023-05-20 22:38:20 +0600

    private route make

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./compo/Home.jsx";
 import ErrorPage from "./compo/ErrorPage.jsx";
-
 import Blog from "./compo/Blog.jsx";
 import Login from "./compo/login/Login.jsx";
 import Signup from "./compo/signUp/Signup.jsx";
 import AuthProvider from "./compo/providers/AuthProvider.jsx";
 import Mytoys from "./compo/Mytoys.jsx";
-
 import Details from "./compo/Details.jsx";
 import PrivateRoute from "./compo/routes/PrivateRoute.jsx";
 import Addtoy from "./compo/Addtoy.jsx";
 import Alltoy from "./compo/Alltoy.jsx";
 import Toydetails from "./compo/Toydetails.jsx";
 import Update from "./compo/Update.jsx";
-
-
-
-
-
 
 const router = createBrowserRouter([
   {
@@ -32,74 +25,74 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element:<Home></Home>
+        element: <Home></Home>,
       },
       {
         path: "/blog",
-        element:<Blog></Blog>
+        element: <Blog></Blog>,
       },
 
       {
-        path: '/login', 
-        element: <Login></Login>
-    }, 
-    {
-        path: '/signup', 
-        element: <Signup></Signup>
-    },
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
 
+      {
+        path: "/alltoy",
+        element: <Alltoy></Alltoy>,
+        loader: () => fetch("https://el-server.vercel.app/bookings"),
+      },
 
-    {
-      path: "/alltoy",
-      element: <Alltoy></Alltoy>,
-      loader: ()=> fetch('https://el-server.vercel.app/bookings')
-    },
+      {
+        path: "/toydetails/:id",
+        element: <Toydetails></Toydetails>,
+        loader: () => fetch("https://el-server.vercel.app/bookings"),
+      },
 
-    {
-      path: '/toydetails/:id', 
-      element: <Toydetails></Toydetails>,
-      loader: ()=> fetch('https://el-server.vercel.app/bookings')
-    },
+      {
+        path: "/bookings",
+        element: (
+          <PrivateRoute>
+            <Mytoys></Mytoys>
+          </PrivateRoute>
+        ),
+      },
 
-    {
-      path: '/bookings', 
-      element: <PrivateRoute><Mytoys></Mytoys></PrivateRoute>
-    },
+      {
+        path: "/addtoy",
+        element: (
+          <PrivateRoute>
+            <Addtoy></Addtoy>
+          </PrivateRoute>
+        ),
+      },
 
+      {
+        path: "/details/:id",
+        element: (
+          <PrivateRoute>
+            <Details></Details>
+          </PrivateRoute>
+        ),
+      },
 
-    {
-      path: "/addtoy",
-      element: <Addtoy></Addtoy>
-    },
-    
-    {
-      path: "/details/:id",
-      element: <PrivateRoute><Details></Details></PrivateRoute> 
-    },
-
-    {
-      path: "/updatetoy/:id",
-      element: <Update></Update>,
-      loader: () => fetch('https://el-server.vercel.app/bookings')
-      
-
-    },
-
-
-
-
-
-
-
-
+      {
+        path: "/updatetoy/:id",
+        element: <Update></Update>,
+        loader: () => fetch("https://el-server.vercel.app/bookings"),
+      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <AuthProvider>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>
 );
